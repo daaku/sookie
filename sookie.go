7667e5d9b407c909541fd10bc637026b2454d8ba
Naitik Shah <n@daaku.org>
2025-10-27 18:26:13 +0400

    Seal/Open methods to use the implementation without http.Cookie directly

--- sookie.go
+++ sookie.go
@@ -29,6 +29,72 @@ type wrapper[V any] struct {
 	E int64
 }
 
+// Seal encodes a Value. The value is encrypted and compressed
+// using the XChaCha20-Poly1305 AEAD algorithm and Zstandard compression.
+// The expiry time, if non-zero will be used when Opening the value to ensure it has not expired.
+func Seal[V any](secret []byte, expires time.Time, value V) (string, error) {
+	var e int64 = -1
+	if !expires.IsZero() {
+		e = expires.Unix()
+	}
+
+	msgp, err := msgpack.Marshal(wrapper[V]{V: value, E: e})
+	if err != nil {
+		return "", fmt.Errorf("sookie: failed to marshal value: %w", err)
+	}
+
+	compressed := encoder.EncodeAll(msgp, nil)
+
+	aead, err := chacha20poly1305.NewX(secret)
+	if err != nil {
+		return "", fmt.Errorf("sookie: failed to create AEAD: %w", err)
+	}
+
+	// initial size is nonce for rand.Read, but capacity for the whole thing
+	nonce := make([]byte, chacha20poly1305.NonceSizeX,
+		chacha20poly1305.NonceSizeX+len(compressed)+chacha20poly1305.Overhead)
+	if _, err := rand.Read(nonce); err != nil {
+		return "", fmt.Errorf("sookie: failed to read nonce: %w", err)
+	}
+	ciphertext := aead.Seal(nonce, nonce, compressed, nil)
+	return base64.RawURLEncoding.EncodeToString(ciphertext), nil
+}
+
+// Open retrieves a value from the raw encrypted string.
+// The raw value is decrypted and decompressed using the XChaCha20-Poly1305 AEAD algorithm.
+// The raw value is unmarshaled into the given type V.
+// If the raw value is expired, the ErrExpired error is returned.
+func Open[V any](secret []byte, raw string) (V, error) {
+	var w wrapper[V]
+	message, err := base64.RawURLEncoding.DecodeString(raw)
+	if err != nil {
+		return w.V, fmt.Errorf("sookie: failed to decode cookie: %w", err)
+	}
+	if len(message) < chacha20poly1305.NonceSizeX {
+		return w.V, errors.New("sookie: invalid cookie length")
+	}
+	nonce, ciphertext := message[:chacha20poly1305.NonceSizeX], message[chacha20poly1305.NonceSizeX:]
+	aead, err := chacha20poly1305.NewX(secret)
+	if err != nil {
+		return w.V, fmt.Errorf("sookie: failed to create AEAD: %w", err)
+	}
+	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return w.V, fmt.Errorf("sookie: failed to decrypt cookie: %w", err)
+	}
+	uncompressed, err := decoder.DecodeAll(plaintext, nil)
+	if err != nil {
+		return w.V, fmt.Errorf("sookie: failed to decompress cookie: %w", err)
+	}
+	if err := msgpack.Unmarshal(uncompressed, &w); err != nil {
+		return w.V, fmt.Errorf("sookie: failed to unmarshal cookie: %w", err)
+	}
+	if w.E != -1 && time.Now().Unix() > w.E {
+		return w.V, ErrExpired
+	}
+	return w.V, nil
+}
+
 // Set sets a cookie with the given value. The value is encrypted and compressed
 // using the XChaCha20-Poly1305 AEAD algorithm and Zstandard compression.
 // The cookie is set with the given name and options.
@@ -47,32 +113,18 @@ func Set[V any](secret []byte, w http.ResponseWriter, value V, cookie http.Cooki
 		return nil
 	}
 
-	var expires int64 = -1
+	var expires time.Time
 	if cookie.MaxAge > 0 {
-		expires = time.Now().Add(time.Duration(cookie.MaxAge) * time.Second).Unix()
+		expires = time.Now().Add(time.Duration(cookie.MaxAge) * time.Second)
 	} else if !cookie.Expires.IsZero() {
-		expires = cookie.Expires.Unix()
+		expires = cookie.Expires
 	}
-	msgp, err := msgpack.Marshal(wrapper[V]{V: value, E: expires})
+
+	encoded, err := Seal(secret, expires, value)
 	if err != nil {
-		return fmt.Errorf("sookie: failed to marshal value: %w", err)
+		return err
 	}
-
-	compressed := encoder.EncodeAll(msgp, nil)
-
-	aead, err := chacha20poly1305.NewX(secret)
-	if err != nil {
-		return fmt.Errorf("sookie: failed to create AEAD: %w", err)
-	}
-
-	// initial size is nonce for rand.Read, but capacity for the whole thing
-	nonce := make([]byte, chacha20poly1305.NonceSizeX,
-		chacha20poly1305.NonceSizeX+len(compressed)+chacha20poly1305.Overhead)
-	if _, err := rand.Read(nonce); err != nil {
-		return fmt.Errorf("sookie: failed to read nonce: %w", err)
-	}
-	ciphertext := aead.Seal(nonce, nonce, compressed, nil)
-	cookie.Value = base64.RawURLEncoding.EncodeToString(ciphertext)
+	cookie.Value = encoded
 
 	if err := cookie.Valid(); err != nil {
 		return fmt.Errorf("sookie: invalid cookie: %w", err)
@@ -101,39 +153,13 @@ func Del(w http.ResponseWriter, r *http.Request, cookie http.Cookie) {
 // If the cookie is not found, the http.ErrNoCookie error is returned.
 // If the cookie is expired, the ErrExpired error is returned.
 func Get[V any](secret []byte, r *http.Request, name string) (V, error) {
-	var w wrapper[V]
 	cookie, err := r.Cookie(name)
 	if err != nil {
+		var v V
 		if err == http.ErrNoCookie {
-			return w.V, err
+			return v, err
 		}
-		return w.V, fmt.Errorf("sookie: failed to get cookie: %w", err)
+		return v, fmt.Errorf("sookie: failed to get cookie: %w", err)
 	}
-	message, err := base64.RawURLEncoding.DecodeString(cookie.Value)
-	if err != nil {
-		return w.V, fmt.Errorf("sookie: failed to decode cookie: %w", err)
-	}
-	if len(message) < chacha20poly1305.NonceSizeX {
-		return w.V, errors.New("sookie: invalid cookie length")
-	}
-	nonce, ciphertext := message[:chacha20poly1305.NonceSizeX], message[chacha20poly1305.NonceSizeX:]
-	aead, err := chacha20poly1305.NewX(secret)
-	if err != nil {
-		return w.V, fmt.Errorf("sookie: failed to create AEAD: %w", err)
-	}
-	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		return w.V, fmt.Errorf("sookie: failed to decrypt cookie: %w", err)
-	}
-	uncompressed, err := decoder.DecodeAll(plaintext, nil)
-	if err != nil {
-		return w.V, fmt.Errorf("sookie: failed to decompress cookie: %w", err)
-	}
-	if err := msgpack.Unmarshal(uncompressed, &w); err != nil {
-		return w.V, fmt.Errorf("sookie: failed to unmarshal cookie: %w", err)
-	}
-	if w.E != -1 && time.Now().Unix() > w.E {
-		return w.V, ErrExpired
-	}
-	return w.V, nil
+	return Open[V](secret, cookie.Value)
 }
